9f5176881a1b355a1f30b6ac9c881851dc087098
Thyrail <atheosc18@gmail.com>
2025-02-05 09:34:56 +0100

    bug fix, s letter on line 9 user.model

--- db/models/user.model.js
+++ db/models/user.model.js
@@ -6,4 +6,4 @@ export const User = sequelize.define('User', {
     name: { type: DataTypes.STRING, allowNull: false },
     email: { type: DataTypes.STRING, unique: true, allowNull: false },
     password: { type: DataTypes.STRING, allowNull: false },
-});s
+});
